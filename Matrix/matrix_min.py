758a5fb9fc14e5e8138a40e561101e7a4f2492a6
Jannis Köckritz <jannis.koeckritz@gmx.de>
2019-03-27 17:22:24 +0100

    Add matrix task

---
+++ Matrix/matrix_min.py
@@ -0,0 +1,67 @@
+# define the input matrix in the following format
+input = [   [3,7,1,5],
+            [6,9,2,10],
+            [11,5,37,3],
+            [2,3,1,0] ]
+
+# method that gets the matrix and a cell coordinate and 
+# returns the minimal cost with the corresponding path 
+def find_path(matrix, goal):
+    cost_matrix = [[0 for x in range(goal[0])] for y in range(goal[1])]
+    path = [[0 for x in range(goal[0])] for y in range(goal[1])]
+    cost_matrix[0][0] = matrix[0][0]
+    path[0][0] = (0,0)
+
+    #getting minimal cost of first column
+    for i in range(1,goal[0]):
+        aboveCost = cost_matrix[i-1][0]
+        cost_matrix[i][0] = aboveCost + matrix[i][0]  + 10
+        path[i][0] = (i-1,0)
+    
+    #getting minimal cost of first row
+    for j in range(1,goal[1]):
+        leftCost = cost_matrix[0][j-1]
+        cost_matrix[0][j] = leftCost + matrix[0][j]  + 10
+        path[0][j] = (0,j-1)
+    
+    #getting minimal cost for the other cells
+    for i in range(1,goal[0]):
+        for j in range(1,goal[1]):
+            leftCost = cost_matrix[i-1][j] + 10
+            aboveCost = cost_matrix[i][j-1] + 10
+            diagonalCost = cost_matrix[i-1][j-1]
+            
+            if min(leftCost, aboveCost, diagonalCost) == leftCost:
+                cost_matrix[i][j] = matrix[i][j] + leftCost
+                path[i][j] = (i-1,j)
+            elif min(leftCost, aboveCost, diagonalCost) == aboveCost:
+                cost_matrix[i][j] = matrix[i][j] + aboveCost
+                path[i][j] = (i,j-1)
+            else:
+                cost_matrix[i][j] = matrix[i][j] + diagonalCost
+                path[i][j] = (i-1,j-1)
+
+    #getting the shortest path from the path matrix
+    x = goal[0]-1
+    y = goal[1]-1
+    liste = []
+    liste.append((x,y))
+    while not x == 0 and not y == 0:
+        x = liste[-1][0]
+        y = liste[-1][1]
+        liste.append(path[x][y])
+    print('Der minimale Aufwand beträgt {}, bei folgendem Weg: '.format(str(cost_matrix[goal[0]-1][goal[1]-1])))
+    for element in list(reversed(liste)):
+        print(element)
+    
+    '''
+    for getting the minimal cost_matrix and 
+    the path_matrix printed,comment the code below out
+    '''
+    # for row in cost_matrix:
+    #     print(row)
+    # for row in path:
+    #     print(row)
+find_path(input,(4,4)) # method gets matrix and goal
+
+    
