ed2ea8a59c2df23193d17b542b93b57159119015
Jannis Köckritz <jannis.koeckritz@gmx.de>
2019-04-15 20:57:06 +0200

    Update description

--- Matrix/dynamic_program_matrix.py
+++ Matrix/dynamic_program_matrix.py
@@ -3,6 +3,11 @@ import time
 import argparse
 import numpy as np
 
+"""
+The following block allows to pass the arguments 'size' (-s/--size) and 'variance' (-v/--variance) 
+through the command-line to the script. They are optional.
+Usage: 'python3 dynamic_program_matrix.py -s 10 -v 20' for 10x10 matrix with values from 0 to 20.
+"""
 parser = argparse.ArgumentParser(description='Get information about matrix')
 parser.add_argument('-s','--size',help='Size', required=False)
 parser.add_argument('-v','--variance',help='Variance', required=False)
@@ -19,43 +24,45 @@ try:
 except ValueError:
     print("Please enter a valid number")
 
-def createMatrix(x=10,varianz=10):
-    return np.array(np.random.randint(0,varianz+1,size=(int(x),int(x))))
+#Function creates random matrix with optional parameters (size and variance) 
+def createMatrix(s=10,varianz=10):
+    return np.array(np.random.randint(0,varianz+1,size=(int(s),int(s))))
 
-def createHomoMatrix(wert=99999999,x=10):
-    return [[wert for x in range(0,x)] for y in range(0,x)]
+#Function creates scoring matrix with the same size as matrix created above. All cells get th same value ("*")
+def createHomoMatrix(s=10,value=99999999):
+    return [[value for x in range(0,s)] for y in range(0,s)]
 
-def beamter(x,y,wert):
-    if x == 0 and y ==0:
-        werte[y][x]=labyrinth[y][x]
-        wert=0
+#Core function. Works through recursion.
+def calcValue(x,y,value):
+    if x == 0 and y ==0: #
+        scores[y][x]=maze[y][x]
+        value=0
     else:
         if y >0 and x >0:
-            if werte[y][x]=="*":
-                wert=min(beamter(x,y-1,wert)[2],beamter(x-1,y,wert)[2],beamter(x-1,y-1,wert)[2])
-                werte[y][x]=wert+labyrinth[y][x]
-            else: wert=werte[y][x]
+            if scores[y][x]=="*":
+                value=min(calcValue(x,y-1,value)[2],calcValue(x-1,y,value)[2],calcValue(x-1,y-1,value)[2])
+                scores[y][x]=value+maze[y][x]
+            else: value=scores[y][x]
         elif x == 0:
-            if werte[y][0]=="*":
-                wert=beamter(x,y-1,wert)[2]
-                werte[y][0]=wert+labyrinth[y][0]
-            else: wert=werte[y][0]
+            if scores[y][0]=="*":
+                value=calcValue(x,y-1,value)[2]
+                scores[y][0]=value+maze[y][0]
+            else: value=scores[y][0]
         elif y == 0:
-            if werte[0][x]=="*":
-                wert= beamter(x-1,y,wert)[2]
-                werte[0][x]=wert+labyrinth[0][x]
-            else: wert=werte[0][x]
-    posWert=wert+labyrinth[y][x]
-    return [x,y,posWert]
+            if scores[0][x]=="*":
+                value= calcValue(x-1,y,value)[2]
+                scores[0][x]=value+maze[0][x]
+            else: value=scores[0][x]
+    posValue=value+maze[y][x]
+    return [x,y,posValue]
 
-labyrinth=createMatrix(int(isize),int(ivar))
-print(labyrinth)
-werte=createHomoMatrix("*",int(isize))
+maze=createMatrix(int(isize),int(ivar))
+print(maze)
+scores=createHomoMatrix(int(isize),"*")
 time1=time.time()
-beamter(len(labyrinth)-1,len(labyrinth)-1,0)
+calcValue(len(maze)-1,len(maze)-1,0)
 time2=time.time()
 
-# find_path(werte)
-for zeile in werte:
+for zeile in scores:
     print(zeile)
-print("Dauer: ",time2-time1)
+print("Duration: ",time2-time1)
